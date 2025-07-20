13932d6ecf4620faa8100931b01f53d61446a7f6
izz <inelmrex611@gmail.com>
2025-07-20 20:20:35 +0800

    fix retrofit problem

--- app/src/main/java/com/example/groupproject/remote/ApiUtils.java
+++ app/src/main/java/com/example/groupproject/remote/ApiUtils.java
@@ -3,11 +3,10 @@ package com.example.groupproject.remote;
 public class ApiUtils {
     public static final String BASE_URL = "https://codelah.my/2024916785/api/";
 
-    // return UserService instance
+    // Return UserService instance
+    public static UserService getUserService() {
         return RetrofitClient.getClient(BASE_URL).create(UserService.class);
     }
 
-    }
 
-    }*/
 }
